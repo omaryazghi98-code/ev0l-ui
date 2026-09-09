5874936773ad52cdac7e76c4bff0409875a22e64
omaryazghi98-code <omaryazghi98@gmail.com>
2026-09-09 03:21:05 +0100

    fix: mount status text cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import AppDitherBackground from './components/AppDitherBackground'
 import PlayerPicker from './components/PlayerPicker'
 import ProfileGuardAgent from './components/ProfileGuardAgent'
 import SimklSyncAgent from './components/SimklSyncAgent'
+import StatusTextCleanup from './components/StatusTextCleanup'
 import { HomePage, IPTVPage, IPTVWatchPage, ListingPage, MyListPage, NotFoundPage, SearchPage, SportsPage, SportsWatchPage, StreamingPage, StatusPage, TitlePage, WatchPage } from './pages/Pages'
 import SettingsPage from './pages/SettingsPage'
 import SimklPage from './pages/SimklPage'
@@ -11,7 +12,7 @@ import './styles/globals.css'
 import './styles/viewport-fixes.css'
 
 export default function App() {
-  return <BrowserRouter><RootShell><AppDitherBackground/><ProfileGuardAgent/><PlayerPicker/><SimklSyncAgent/><Routes>
+  return <BrowserRouter><RootShell><AppDitherBackground/><ProfileGuardAgent/><PlayerPicker/><SimklSyncAgent/><StatusTextCleanup/><Routes>
     <Route path="/" element={<HomePage/>}/>
     <Route path="/movies" element={<ListingPage type="movie"/>}/>
     <Route path="/series" element={<ListingPage type="series"/>}/>
